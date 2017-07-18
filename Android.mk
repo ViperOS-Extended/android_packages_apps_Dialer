c33bb692de90f11ab26872d0368194084f7e3c27
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-07-18 20:27:04 -0300

    Dialer: Viper Color Engine

--- Android.mk
+++ Android.mk
@@ -39,6 +39,10 @@ LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs)) \
     $(support_library_root_dir)/v7/appcompat/res \
     $(support_library_root_dir)/design/res
 
+# Viper Color Engine
+$(warning *** Including Viper Color Engine ***)
+LOCAL_RESOURCE_DIR += vendor/viper/colorengine/$(LOCAL_PATH)/res
+
 LOCAL_ASSET_DIR += $(LOCAL_PATH)/assets
 
 LOCAL_JAVA_LIBRARIES := telephony-common \
